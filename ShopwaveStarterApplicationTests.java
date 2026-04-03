b523a208f0aaa2c6145841c27e876bd4d4e365a9
TeshFen <barakliyu@gmail.com>
2026-04-03 23:58:02 +0300

    C5- integration testing

---
+++ ShopwaveStarterApplicationTests.java
@@ -0,0 +1,16 @@
+//Fenet Teshome
+//ATE/9860/14
+
+package com.shopwave.shopwave_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopwaveStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
